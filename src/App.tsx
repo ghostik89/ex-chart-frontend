1b393801d7b8ec38dcc4fa83c1107e5938d11913
Lady_Proger <facktartar@mail.com>
2021-04-04 22:43:16 +0300

    init react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {useChart} from "./hooks/useChart";
+import {Layout, Typography} from "antd";
+import {DateSlider} from "./components/DateSlider";
+import 'antd/dist/antd.css';
+import {ChartTable} from "./components/ChartTable";
+import {IdeChart} from "./components/IdeChart";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {chart, isError, isLoading} = useChart()
+    const [interval, setInterval] = useState({})
+    const {Title, Paragraph} = Typography
+    const {Content} = Layout
+
+    return (
+        <>
+            <Layout>
+                <Layout>
+                    <Content>
+                        <Title>Stats</Title>
+                        <Paragraph>From som to som</Paragraph>
+                        <DateSlider/>
+                        <ChartTable/>
+                        {['InteliJ IDEA', 'WebStorm', 'PhpStorm'].map((elem, index) => (
+                            <IdeChart ideName={elem} key={index}/>
+                        ))}
+                    </Content>
+                </Layout>
+            </Layout>
+        </>
+    );
 }
 
 export default App;
