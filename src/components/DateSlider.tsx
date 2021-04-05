2ca1462a50984b79a59afcd41b60677c77ec1cfe
Lady_Proger <facktartar@mail.com>
2021-04-05 15:26:44 +0300

    add handlers

--- src/components/DateSlider.tsx
+++ src/components/DateSlider.tsx
@@ -1,39 +1,79 @@
 import React from "react";
 import {useChart} from "../hooks/useChart";
-import {Button, Col, Row, Select} from "antd";
+import {Button, Col, Row, Select, Skeleton} from "antd";
 import {LeftCircleOutlined, RightCircleOutlined} from "@ant-design/icons";
+import {useDateRange} from "../context/DatesContext";
+import {add, sub} from "date-fns";
 
 export const DateSlider = () => {
-    const {chart, isError, isLoading} = useChart()
+    const {changeRangeType, dateRange, changeStartDate, fromDate} = useDateRange()
+    const {isLoading} = useChart()
     const { Option } = Select;
+
+    const handleChange = (value:string) => {changeRangeType(value)}
+
+    const handleAdd = () => {
+        switch (dateRange) {
+            case 'quarter':
+                changeStartDate(add(fromDate, {months: 3}))
+                break
+            case 'month':
+                changeStartDate(add(fromDate, {months: 1}))
+                break
+            case 'week':
+                changeStartDate(add(fromDate, {days: 7}))
+                break
+            case 'day':
+                changeStartDate(add(fromDate, {days: 1}))
+                break
+        }
+    }
+
+    const handleSub = () => {
+        switch (dateRange) {
+            case 'quarter':
+                changeStartDate(sub(fromDate, {months: 3}))
+                break
+            case 'month':
+                changeStartDate(sub(fromDate, {months: 1}))
+                break
+            case 'week':
+                changeStartDate(sub(fromDate, {days: 7}))
+                break
+            case 'day':
+                changeStartDate(sub(fromDate, {days: 1}))
+                break
+        }
+    }
 
     return(
         <Row>
+                <Col span={8}>
+                    {isLoading? <Skeleton/>:<Button
+                        type="primary"
+                        icon={<LeftCircleOutlined/>}
+                        onClick={handleAdd}
+                    >
+                        {`Previous ${dateRange}`}
+                    </Button>}
+                </Col>
             <Col span={8}>
-                <Button
-                    type="primary"
-                    icon={<LeftCircleOutlined />}
-                >
-                    Click me!
-                </Button>
+                {isLoading? <Skeleton/>:
+                    <Select value={dateRange} defaultValue="quarter" onChange={handleChange}>
+                        <Option value="quarter">One quarter</Option>
+                        <Option value="month">Month</Option>
+                        <Option value="week">Week</Option>
+                        <Option value="day">Day</Option>
+                    </Select>}
             </Col>
             <Col span={8}>
-                <Select defaultValue="lucy">
-                    <Option value="jack">Jack</Option>
-                    <Option value="lucy">Lucy</Option>
-                    <Option value="disabled" disabled>
-                        Disabled
-                    </Option>
-                    <Option value="Yiminghe">yiminghe</Option>
-                </Select>
-            </Col>
-            <Col span={8}>
-                <Button
+                {isLoading? <Skeleton/>:<Button
                     type="primary"
-                    icon={<RightCircleOutlined />}
+                    icon={<RightCircleOutlined/>}
+                    onClick={handleSub}
                 >
-                    Click me!
-                </Button>
+                    {`Next ${dateRange}`}
+                </Button>}
             </Col>
         </Row>
     )
