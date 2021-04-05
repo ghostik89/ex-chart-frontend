79d18b742600b1f82be929e61559f5515a9b8e37
Lady_Proger <facktartar@mail.com>
2021-04-05 23:09:50 +0300

    add responsibility container and title

--- src/components/IdeChart.tsx
+++ src/components/IdeChart.tsx
@@ -6,8 +6,9 @@ import {
     YAxis,
     CartesianGrid,
     Tooltip,
-    Legend
+    Legend, ResponsiveContainer
 } from "recharts";
+import {Typography} from "antd";
 
 const data = [
     {
@@ -55,25 +56,30 @@ const data = [
 ];
 
 export const IdeChart = ({ideName}:{ideName:string}) => {
+    const {Title} = Typography
+
     return (
-        <BarChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="pv" fill="#8884d8" />
-            <Bar dataKey="uv" fill="#82ca9d" />
-        </BarChart>
+        <>
+            <Title>{ideName}</Title>
+            <ResponsiveContainer width="100%" height={300}>
+                <BarChart
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="pv" fill="#8884d8" />
+                    <Bar dataKey="uv" fill="#82ca9d" />
+                </BarChart>
+            </ResponsiveContainer>
+        </>
     );
 }
