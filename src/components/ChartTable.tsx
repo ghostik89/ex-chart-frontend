5a7e3d8b8ea96762302ce7c94faf8be8ae70861f
Lady_Proger <facktartar@mail.com>
2021-04-05 15:28:15 +0300

    add handlers

--- src/components/ChartTable.tsx
+++ src/components/ChartTable.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Space, Table, Tag} from "antd";
+import {useChart} from "../hooks/useChart";
+import {useDateRange} from "../context/DatesContext";
+import {isWithinInterval} from "date-fns";
 
 
 export const ChartTable = () => {
+    const {chart, isError, isLoading} = useChart()
+    const {dateRange, fromDate, toDate} = useDateRange()
+    const [data, setData] = useState<any>([])
+
     const columns = [
         {
             title: 'Product',
@@ -25,14 +32,24 @@ export const ChartTable = () => {
             dataIndex: 'averageUsage',
         },
     ];
-    const data = [
-        {
-            key: '1',
-            product: 'webstorm',
-            minUsage: 32,
-            maxUsage: 26,
-            averageUsage: 15
-        },
-    ];
+
+    const configWebstorm = () => {
+        chart.filter((elem: { timestamp: string | number | Date; }) => isWithinInterval(new Date(elem.timestamp),
+            {start: fromDate, end: toDate}))
+    }
+
+    useEffect(() => {
+        setData(['InteliJ IDEA', 'WebStorm', ''])
+    },[dateRange, fromDate])
+
+    // const data = [
+    //     {
+    //         key: '1',
+    //         product: 'webstorm',
+    //         minUsage: 32,
+    //         maxUsage: 26,
+    //         averageUsage: 15
+    //     },
+    // ];
     return(<Table columns={columns} dataSource={data} />)
 }
