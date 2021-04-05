89d16d959112a26b3165d1b34e0686c4f33f3b2e
Lady_Proger <facktartar@mail.com>
2021-04-05 23:09:18 +0300

    add func for work with data

--- src/components/ChartTable.tsx
+++ src/components/ChartTable.tsx
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from 'react'
-import {Space, Table, Tag} from "antd";
+import {Table} from "antd";
 import {useChart} from "../hooks/useChart";
 import {useDateRange} from "../context/DatesContext";
 import {isWithinInterval} from "date-fns";
 
 
 export const ChartTable = () => {
-    const {chart, isError, isLoading} = useChart()
+    const {chart, isLoading} = useChart()
     const {dateRange, fromDate, toDate} = useDateRange()
     const [data, setData] = useState<any>([])
 
@@ -34,22 +34,64 @@ export const ChartTable = () => {
     ];
 
     const configWebstorm = () => {
-        chart.filter((elem: { timestamp: string | number | Date; }) => isWithinInterval(new Date(elem.timestamp),
+        const temp = chart
+            .filter((elem: { timestamp: string | number | Date; }) => isWithinInterval(new Date(elem.timestamp),
             {start: fromDate, end: toDate}))
+            .map((elem: { webstorm: number | null; }) => elem.webstorm)
+            .filter((elem: null | number) => elem !== null)
+        const minUsage = Math.min.apply(null, temp)
+        const maxUsage = Math.max.apply(null, temp)
+        const averageUsage = temp.reduce((a: any, b: any) => a + b, 0) / temp.length || 0;
+
+        return { minUsage, maxUsage, averageUsage }
+    }
+
+    const configIdea = () => {
+        const temp = chart
+            .filter((elem: { timestamp: string | number | Date; }) => isWithinInterval(new Date(elem.timestamp),
+                {start: fromDate, end: toDate}))
+            .map((elem: { idea: number | null; }) => elem.idea)
+            .filter((elem: null | number) => elem !== null)
+        const minUsage = Math.min.apply(null, temp)
+        const maxUsage = Math.max.apply(null, temp)
+        const averageUsage = temp.reduce((a: any, b: any) => a + b, 0) / temp.length || 0;
+
+        return { minUsage, maxUsage, averageUsage }
+    }
+
+    const configPhpStorm = () => {
+        const temp = chart
+            .filter((elem: { timestamp: string | number | Date; }) => isWithinInterval(new Date(elem.timestamp),
+                {start: fromDate, end: toDate}))
+            .map((elem: { goland: number | null; }) => elem.goland)
+            .filter((elem: null | number) => elem !== null)
+        const minUsage = Math.min.apply(null, temp)
+        const maxUsage = Math.max.apply(null, temp)
+        const averageUsage = temp.reduce((a: any, b: any) => a + b, 0) / temp.length || 0;
+
+        return { minUsage, maxUsage, averageUsage }
     }
 
     useEffect(() => {
-        setData(['InteliJ IDEA', 'WebStorm', ''])
-    },[dateRange, fromDate])
+        if(!isLoading)
+            setData([
+                {
+                    key: '1',
+                    product: 'InteliJ IDEA',
+                    ...configIdea()
+                },
+                {
+                    key: '2',
+                    product: 'Webstorm',
+                    ...configWebstorm()
+                },
+                {
+                    key: '3',
+                    product: 'Phpstorm',
+                    ...configPhpStorm()
+                }
+            ])
+    },[isLoading, dateRange, fromDate])
 
-    // const data = [
-    //     {
-    //         key: '1',
-    //         product: 'webstorm',
-    //         minUsage: 32,
-    //         maxUsage: 26,
-    //         averageUsage: 15
-    //     },
-    // ];
-    return(<Table columns={columns} dataSource={data} />)
+    return(<Table columns={columns} dataSource={data} loading={isLoading}/>)
 }
